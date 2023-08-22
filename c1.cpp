9208f6bc92c622a08a5a11647ac9a44dc28041bc
DUSTFENIR <142887672+DUSTFENIR@users.noreply.github.com>
2023-08-22 17:35:02 +0800

    Add files via upload

---
+++ c1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	int a=1;
+	int b=5;
+	int c=6;
+	double x1; 
+	double x2;
+	double d;
+	d=b*b-4*a*c;
+	if (d>0)
+	{
+		x1=(-b+sqrt(d))/2*a;
+		x2=(-b-sqrt(d))/2*a;
+		printf("x1=%f,x2=%f\n",x1,x2);
+	}
+	else if(d==0)
+	{
+		x1=(-b)/2*a;
+		x2=x1;
+		printf("x1=x2=%f",x1) ;
+	} 
+	else
+	{
+		printf("нч╫Б") ;
+	} 
+	return 0;
+}
+
